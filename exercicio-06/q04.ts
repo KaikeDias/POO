5bda9c4e790b9b7b05badd2bcb1d89421c4f4f2a
Kaike Dias <kaikedias98@gmail.com>
2022-10-12 16:10:02 -0300

    Exercicio 06

--- exercicio-06/q04.ts
+++ exercicio-06/q04.ts
@@ -5,5 +5,5 @@ let conta: Conta = new Conta('1', 100);
 //console.log(conta.numero); aparece: Property 'numero' is private and only accessible within class 'Conta'.
 //console.log(conta.saldo); aparece: Property 'saldo' is private and only accessible within class 'Conta'.
 
-console.log(conta.getNumero());
-console.log(conta.getSaldo());
+console.log(conta.Numero);
+console.log(conta.Saldo);
